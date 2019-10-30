b1a8dc357282423422fa1673a1d39b1b0208e498
Brandon Nichols <brandonaronnichols@gamil.com>
2019-10-30 14:49:46 -0500

    added delete request to database to delete a specified user

--- index.js
+++ index.js
@@ -50,3 +50,18 @@ server.get('/api/users/:id', (req, res) => {
             res.status(500).json({ error: "The user information could not be retrieved." });
         })
 })
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+        .then(user => {
+            if(user)
+                res.status(200).end;
+            else
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user could not be removed" });
+        })
+})
