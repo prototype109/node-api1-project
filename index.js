68a78ee6892d1d01e5437f676cce812d3a15d550
Brandon Nichols <brandonaronnichols@gamil.com>
2019-10-30 14:14:46 -0500

    added get request to retrieve all the users as the response

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ server.post('/api/users', (req, res) => {
         })
 
 })
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The users information could not be retrieved." })
+        })
+})
