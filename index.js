6f422503ce83ee1c072b66a2a76004de40e9efdd
Brandon Nichols <brandonaronnichols@gamil.com>
2019-10-30 00:24:21 -0500

    added basic setup as well as a post request that adds a user to the database

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(4000, () => {
+    console.log('Server Listening on http://localhost:4000');
+})
+
+server.post('/api/users', (req, res) => {
+    const reqBody = req.body;
+
+    db.insert(reqBody)
+        .then(user => {
+            res.status(201).json({message: 'successfully created user'})
+        })
+        .catch(err => {
+            res.status(500).json({message: `Failed to add user, ${err}`})
+        })
+
+})
