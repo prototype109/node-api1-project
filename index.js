beb11331fc52288faa4b65178817e94bb8a8a5f3
Brandon Nichols <brandonaronnichols@gamil.com>
2019-10-30 14:44:33 -0500

    added get request for specific user in database

--- index.js
+++ index.js
@@ -35,3 +35,18 @@ server.get('/api/users', (req, res) => {
             res.status(500).json({ error: "The users information could not be retrieved." })
         })
 })
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.findById(id)
+        .then(user => {
+            if(user)
+                res.status(200).json(user);
+            else
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be retrieved." });
+        })
+})
